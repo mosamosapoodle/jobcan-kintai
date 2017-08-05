352885812927a9a6c51329233e1ad498a98baaef
野々目龍太郎 <NonomeRyutaro@NonomenoMacBook-Pro.local>
2017-08-05 09:10:21 +0900

    Refactoring main.rb

--- main.rb
+++ main.rb
@@ -8,14 +8,9 @@ def dakoku(ele_id = '')
   url = "https://ssl.jobcan.jp/login/pc-employee/?client_id=#{ENV['CLIENT_ID']}"
   driver = Selenium::WebDriver.for :chrome
   driver.navigate.to url
-  # ログインフォーム入力
-  driver.find_element(:id, 'email')
-  .send_keys(ENV['EMAIL'])
-  driver.find_element(:id, 'password')
-  .send_keys(ENV['PASS'])
-  # ログインボタン押下
+  driver.find_element(:id, 'email').send_keys(ENV['EMAIL'])
+  driver.find_element(:id, 'password').send_keys(ENV['PASS'])
   driver.find_element(css: '.btn').click
-  # 勤怠関連ボタン押下
   driver.find_element(:id, ele_id).click
   sleep(20)
   driver.quit
